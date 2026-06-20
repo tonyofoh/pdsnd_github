cd16a10cea51b38a61e99b53a989c4480b05b234
Tony Ofoh <ofohelohor@gmail.com>
2026-06-20 04:52:36 +0100

    Create helper function for printing elapsed time

--- bikeshare_starter.py
+++ bikeshare_starter.py
@@ -8,6 +8,10 @@ CITY_DATA = { 'chicago': 'chicago.csv',
 
 SEPARATOR = '-' * 40
 
+def print_elapsed_time(start_time):
+    """Prints how long a calculation took."""
+    elapsed_time = time.time() - start_time
+    print(f"\nThis took {elapsed_time} seconds.")
 
 def get_filters():
     """
@@ -63,7 +67,7 @@ def time_stats(df):
     # display the most common start hour
 
 
-    print("\nThis took %s seconds." % (time.time() - start_time))
+    print_elapsed_time(start_time)
     print(SEPARATOR)
 
 
@@ -82,7 +86,7 @@ def station_stats(df):
     # display most frequent combination of start station and end station trip
 
 
-    print("\nThis took %s seconds." % (time.time() - start_time))
+    print_elapsed_time(start_time)
     print(SEPARATOR)
 
 
@@ -98,7 +102,7 @@ def trip_duration_stats(df):
     # display mean travel time
 
 
-    print("\nThis took %s seconds." % (time.time() - start_time))
+    print_elapsed_time(start_time)
     print(SEPARATOR)
 
 
@@ -117,7 +121,7 @@ def user_stats(df):
     # Display earliest, most recent, and most common year of birth
 
 
-    print("\nThis took %s seconds." % (time.time() - start_time))
+    print_elapsed_time(start_time)
     print(SEPARATOR)
 
 
