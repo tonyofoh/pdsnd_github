9386e7575eb34322a00d1bc8531d6df4041288e5
Tony Ofoh <ofohelohor@gmail.com>
2026-06-20 04:49:05 +0100

    Create a constant for the separator line

--- bikeshare_starter.py
+++ bikeshare_starter.py
@@ -6,6 +6,9 @@ CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
 
+SEPARATOR = '-' * 40
+
+
 def get_filters():
     """
     Asks user to specify a city, month, and day to analyze.
@@ -61,7 +64,7 @@ def time_stats(df):
 
 
     print("\nThis took %s seconds." % (time.time() - start_time))
-    print('-'*40)
+    print(SEPARATOR)
 
 
 def station_stats(df):
@@ -80,7 +83,7 @@ def station_stats(df):
 
 
     print("\nThis took %s seconds." % (time.time() - start_time))
-    print('-'*40)
+    print(SEPARATOR)
 
 
 def trip_duration_stats(df):
@@ -96,7 +99,7 @@ def trip_duration_stats(df):
 
 
     print("\nThis took %s seconds." % (time.time() - start_time))
-    print('-'*40)
+    print(SEPARATOR)
 
 
 def user_stats(df):
@@ -115,7 +118,7 @@ def user_stats(df):
 
 
     print("\nThis took %s seconds." % (time.time() - start_time))
-    print('-'*40)
+    print(SEPARATOR)
 
 
 def main():
